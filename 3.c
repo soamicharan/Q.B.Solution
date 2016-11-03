96ccdcb80be5f9cec81148b7982969d67ea84e68
Soami Charan <charansoami@gmail.com>
2016-11-03 14:27:25 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -3,14 +3,14 @@
 int main()
 {
     int num,sum,rem;
-    sum=0;
+    sum=0;                //we intializing sum with 0
     printf("Enter a number.\n");
     scanf("%d",&num);
-    while(num!=0)
+    while(num!=0)       //initiating a while loop with condition num is not equal to zero
     {
-        rem=num%10;
-        sum=sum+rem;
-        num=num/10;
+        rem=num%10;        //we initializing rem integer in remainder of num by dividing with 10
+        sum=sum+rem;             //we adding remainder with sum integer
+        num=num/10;             //now num is divded by 10
     }
     printf("Sum of digits = %d",sum);
     getch();
